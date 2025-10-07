b6a079d5cfbeba3549935b7733aa42645ccc78d5
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-10-07 09:16:05 -0400

    update

--- scripts/refresh-conversion.sh
+++ scripts/refresh-conversion.sh
@@ -1,5 +1,6 @@
-skewness=2.5
+skewness=1.0
 for i in results/fully-associative/*.json; do
     PROGRAM_NAME=$(basename $i .json)
+    echo "Processing $PROGRAM_NAME with skewness $skewness"
     cargo run --release --quiet --bin assoc-conv -- -o results/12-way/${PROGRAM_NAME}.json -i $i -a 12 -s $skewness
 done
